f798549b55fa7ba9b3042f239203b5f4e744f87c
theaulait <vanice.yee@gmail.com>
2015-05-20 18:00:02 -0400

    initial

--- app/src/main/java/com/example/joe/plus/MainActivity.java
+++ app/src/main/java/com/example/joe/plus/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.joe.plus;
 
+import android.app.Activity;
+import android.content.res.Configuration;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,145 +11,188 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView display = (TextView)findViewById(R.id.display);
+        final TextView display = (TextView) findViewById(R.id.display);
 
-
-        final Button cosbutton = (Button) findViewById(R.id.cos);
-            cosbutton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    String caltext = cosbutton.getText().toString();
-                    display.append(caltext);
-                }
-            });
-
-
-        final Button sinbutton = (Button) findViewById(R.id.sin);
-        sinbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = sinbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
-
-
-        final Button tanbutton = (Button) findViewById(R.id.tan);
-        tanbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                    String caltext = tanbutton.getText().toString();
-                    display.append(caltext);
-                }
-            });
 
         final Button mulbutton = (Button) findViewById(R.id.multiple);
         mulbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String caltext = mulbutton.getText().toString();
-                display.append(caltext);
+                display.append(mulbutton.getText().toString());
             }
         });
 
-        final Button sqrbutton = (Button) findViewById(R.id.square);
-        sqrbutton.setOnClickListener(new View.OnClickListener() {
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button sinbutton = (Button) findViewById(R.id.sin);
+            sinbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (sinbutton != null) {
+                        display.append(sinbutton.getText().toString());
+                    }
+                }
+            });
+        }
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button tanbutton = (Button) findViewById(R.id.tan);
+            tanbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (tanbutton != null) {
+                        display.append(tanbutton.getText().toString());
+                    }
+                }
+            });
+        }
+
+        final Button divdbutton = (Button) findViewById(R.id.divide);
+        divdbutton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String caltext = sqrbutton.getText().toString();
-                display.append(caltext);
+                if (divdbutton != null) {
+                    display.append(divdbutton.getText().toString());
+                }
             }
         });
 
-        final Button lnbutton = (Button) findViewById(R.id.ln);
-        lnbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = lnbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button sqrbutton = (Button) findViewById(R.id.square);
+            sqrbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (sqrbutton != null) {
+                        display.append(sqrbutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button logbutton = (Button) findViewById(R.id.log);
-        logbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = logbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button lnbutton = (Button) findViewById(R.id.ln);
+            lnbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (lnbutton != null) {
+                        String caltext = lnbutton.getText().toString();
+                        display.append(caltext);
+                    }
+                }
+            });
+        }
 
-        final Button pibutton = (Button) findViewById(R.id.pi);
-        pibutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = pibutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button logbutton = (Button) findViewById(R.id.log);
+            logbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (logbutton != null) {
+                        display.append(logbutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button degbutton = (Button) findViewById(R.id.Deg);
-        degbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = degbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button pibutton = (Button) findViewById(R.id.pi);
+            pibutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (pibutton != null) {
+                        display.append(pibutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button radbutton = (Button) findViewById(R.id.rad);
-        radbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = radbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button degbutton = (Button) findViewById(R.id.Deg);
+            degbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (degbutton != null) {
+                        display.append(degbutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button ebutton = (Button) findViewById(R.id.BigE);
-        ebutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = ebutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button radbutton = (Button) findViewById(R.id.rad);
+            radbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (radbutton != null) {
+                        display.append(radbutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button openbutton = (Button) findViewById(R.id.openpara);
-        openbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = openbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button ebutton = (Button) findViewById(R.id.BigE);
+            ebutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (ebutton != null) {
+                        display.append(ebutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button endbutton = (Button) findViewById(R.id.endpara);
-        endbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = endbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button openbutton = (Button) findViewById(R.id.openpara);
+            openbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (openbutton != null) {
+                        display.append(openbutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button coshbutton = (Button) findViewById(R.id.cosh);
-        coshbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = coshbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button endbutton = (Button) findViewById(R.id.endpara);
+            endbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (endbutton != null) {
+                        display.append(endbutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button sinhbutton = (Button) findViewById(R.id.sinh);
-        sinhbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = sinhbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button coshbutton = (Button) findViewById(R.id.cosh);
+            coshbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (coshbutton != null) {
+                        display.append(coshbutton.getText().toString());
+                    }
+                }
+            });
+        }
 
-        final Button tanhbutton = (Button) findViewById(R.id.tanh);
-        tanhbutton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String caltext = tanhbutton.getText().toString();
-                display.append(caltext);
-            }
-        });
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button sinhbutton = (Button) findViewById(R.id.sinh);
+            sinhbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (sinhbutton != null) {
+                        display.append(sinhbutton.getText().toString());
+                    }
+                }
+            });
+        }
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            final Button tanhbutton = (Button) findViewById(R.id.tanh);
+            tanhbutton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if (tanhbutton != null) {
+                        display.append(tanhbutton.getText().toString());
+                    }
+                }
+            });
+        }
     }
 
 
